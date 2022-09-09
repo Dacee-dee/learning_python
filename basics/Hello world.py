08d8d1b0ab3b963e1b53cab74adbd91e2c85379c
Dacee-dee <75333265+Dacee-dee@users.noreply.github.com>
2022-09-09 13:49:52 +0530

    Initial commit

---
+++ basics/Hello world.py
@@ -0,0 +1,5 @@
+str = "I want to reverse my string"
+strLength = len(str) + 1
+
+print(str + "\n\n")
+print(str[strLength])
---
+++ basics/input.py
@@ -0,0 +1,4 @@
+name1 = input("firstname,enter your firstname")
+name2 = input("last name,enter your lastname")
+
+print("Hello "+name1, name2)
